83d790ebeb62840378855b5ec936dea7404c534d
Shashikant-15 <shashikant.tanti@nashtechglobal.com>
2023-10-25 16:31:07 +0530

    Add project files.

---
+++ CrudOperationsInDotNetCore/Models/BrandContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsInDotNetCore.Models
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)   // constructor
+        {
+
+            
+        }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
